004dede55b3086b356bb8933512c662817a64b10
Administrator <Administrator@PC-20150626MARP>
2015-08-17 10:56:22 +0800

    onResume

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.test;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -16,7 +16,14 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
 		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
 	}
 
 }
